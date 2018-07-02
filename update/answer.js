39b1a4b3e1b00f5d27873ea6534b110c0e46f129
HanYoungsoo <gglife333@gmail.com>
2018-07-03 00:15:44 +0900

    pass all test case

--- update/answer.js
+++ update/answer.js
@@ -8,6 +8,20 @@ const update = (state, command) => {
     switch (commandKeys[0]) {
       case "$set":
         return command.$set
+      case "$push":
+        return state.concat(command.$push)
+      case "$unshift":
+        const newArr = state.slice()
+        newArr.unshift(...command.$unshift)
+        return newArr
+      case "$merge":
+        return Object.assign({}, state, command.$merge)
+      case "$apply":
+        return command.$apply(state)
+      case "$splice":
+        const newArr2 = state.slice()
+        newArr2.splice(...command.$splice[0])
+        return newArr2
     }
   } else {
     nextState = Object.assign({}, state)
@@ -17,6 +31,7 @@ const update = (state, command) => {
       console.log("each key", key)
       console.log("command key", command[key])
       if (Object.keys(command[key]).length > 0) {
+        console.log("nextState key", nextState[key])
         nextState[key] = update(state[key], command[key])
       } else {
         console.log("nextState does not have command")
